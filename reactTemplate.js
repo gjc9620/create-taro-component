5417997464da628ad672c0f2cea63cea084be0ce
gujiacheng <992758355@qq.com>
2019-04-09 12:03:03 +0800

    yeah

--- reactTemplate.js
+++ reactTemplate.js
@@ -7,13 +7,37 @@ module.exports = function reactTemplate ({
   return `import { ComponentType } from 'react';
 import Taro, { Component } from '@tarojs/taro';
 import { View } from '@tarojs/components';
+//import getImageResourceUrl from '@/AppHitchMP/src/utils/getImageResourceUrl';
+//import freeRideService from '@/AppHitchMP/src/utils/api/freeRideService';
+//import Native from '@/AppHitchMP/src/utils/native/native';
 
 import './${name}.scss';
 
-class ${name} extends Component {
+interface Props {
+
+}
+
+interface State {
+
+}
+
+
+class ${name} extends Component<Props, State> {
+  constructor(props){
+    super(props);
+  }
+
+  static options = {}
+
+  static defaultProps = {};
+
+  state = {};
+
+  componentDidMount(): void {}
+  
   render () {
     return (
-      <View className='${paramCase(className)}'>
+      <View className='${paramCase(name)}'>
       
       </View>
     );
@@ -24,3 +48,4 @@ class ${name} extends Component {
 export default ${name} as ComponentType;
 `
 };
+
